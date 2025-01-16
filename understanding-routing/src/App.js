ded68a2c380dd5ea4aee5a38f215666ba0c589e9
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-15 23:06:59 -0500

    use relative paths

--- understanding-routing/src/App.js
+++ understanding-routing/src/App.js
@@ -24,13 +24,13 @@ export default function App() {
       <h1>Understanding Routing</h1>
       <nav>
           <Link to="/">Home</Link>&nbsp;
-          <Link to="/cats">Cats</Link>&nbsp;
-          <Link to="/about">About</Link>
+          <Link to="cats">Cats</Link>&nbsp;
+          <Link to="about">About</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cats" element={<Cats />} />
-        <Route path="/about" element={<About />} />
+        <Route path="cats" element={<Cats />} />
+        <Route path="about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
