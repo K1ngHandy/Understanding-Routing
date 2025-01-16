464e7ba91449c36ccf27b45df661b6d6ee9999dc
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-15 22:22:57 -0500

    setup router

---
+++ understanding-routing/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>My App</h1>
+      {/* Navigation and Routes */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
